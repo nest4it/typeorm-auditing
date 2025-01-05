2da9c221f1198a9834433752535e250ecb7a92f8
Robert-Jan Kuyper <info@datails.nl>
2025-01-05 21:20:27 +0100

    fix: check first if there is meta data

--- src/utils/reflect.ts
+++ src/utils/reflect.ts
@@ -4,20 +4,16 @@ import type { AuditSubscriberOptions } from '../types';
 export const setMetaData = (
   metadataValue: AuditSubscriberOptions & { historyEntity: Function },
   target: Object,
-  propertyKey: string | symbol = undefined,
 ) => {
-  if (propertyKey) {
-    Reflect.defineMetadata(AUDIT_METADATA_KEY, metadataValue, target, propertyKey);
-  } else {
-    Reflect.defineMetadata(AUDIT_METADATA_KEY, metadataValue, target);
-  }
+  Reflect.defineMetadata(AUDIT_METADATA_KEY, metadataValue, target);
 };
 
 export const getMetaData = (
   target: Object,
-  propertyKey: string | symbol = undefined,
 ): AuditSubscriberOptions & { historyEntity: Function } => {
-  return propertyKey
-    ? Reflect.getMetadata(AUDIT_METADATA_KEY, target, propertyKey)
-    : Reflect.getMetadata(AUDIT_METADATA_KEY, target);
+  if (Reflect.hasMetadata(AUDIT_METADATA_KEY, target)) {
+    return Reflect.getMetadata(AUDIT_METADATA_KEY, target);
+  }
+
+  return null;
 };
